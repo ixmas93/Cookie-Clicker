2ba52af9f38a6f922c0606b4d98c61c67ad48448
Francisco Jose Soriano Ruiz <franbarrabajakh@gmail.com>
2024-01-20 19:17:36 +0100

    feat: Counter class and implementation on scene

---
+++ Assets/Scripts/Vista/Counter.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Controlador;
+
+public class Counter : MonoBehaviour
+{
+    int cookieCounter;
+    TMP_Text counterUI;
+    Presenter presenter;
+
+    private void Awake()
+    {
+        counterUI = GetComponent<TMP_Text>();
+    }
+
+    private void Start()
+    {
+        presenter = FindObjectOfType<Presenter>();
+    }
+
+    private void Update()
+    {
+        counterUI.SetText(presenter.Score.playerTotalCookies.ToString());
+    }
+}
