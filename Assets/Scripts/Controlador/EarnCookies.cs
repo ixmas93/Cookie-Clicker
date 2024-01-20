7a9273b649d83f9ef3d104a3bb2094029e398b52
Geri <ggasconmoline@gmail.com>
2024-01-20 18:21:44 +0100

    feat: Basic Presenter script

---
+++ Assets/Scripts/Controlador/EarnCookies.cs
@@ -0,0 +1,11 @@
+using Modelo;
+
+namespace Controlador
+{
+    public class EarnCookies
+    {
+        private readonly Score _score;
+
+        public EarnCookies(Score score) => _score = score;
+    }
+}
