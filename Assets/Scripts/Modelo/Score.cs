dd9732251061c3df14a4cfc5199671a02b68c693
ixmas93 <44982516+ixmas93@users.noreply.github.com>
2024-01-20 17:47:24 +0100

    Feat: Score model

---
+++ Assets/Scripts/Modelo/Score.cs
@@ -0,0 +1,11 @@
+
+public class Score
+{
+    public int playerTotalCookies { get; private set; }
+
+
+    public void AddCookies(int newCookiesAmmount)
+    {
+        playerTotalCookies += newCookiesAmmount;
+    }
+}
