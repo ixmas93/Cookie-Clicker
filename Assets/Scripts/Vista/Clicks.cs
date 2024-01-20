a96222adc4e42bb71bb39565e67e90bcfcbfbedd
Francisco Jose Soriano Ruiz <franbarrabajakh@gmail.com>
2024-01-20 18:30:53 +0100

    feat: add Clicks class

---
+++ Assets/Scripts/Vista/Clicks.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clicks : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        
+    }
+}
