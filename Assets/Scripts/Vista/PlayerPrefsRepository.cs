33282bc9175f875538c9ce6641cd52df71336d39
ixmas93 <44982516+ixmas93@users.noreply.github.com>
2024-02-10 18:31:20 +0100

    Feat: Load Game

--- Assets/Scripts/Vista/PlayerPrefsRepository.cs
+++ Assets/Scripts/Vista/PlayerPrefsRepository.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 namespace Vista {
 	public class PlayerPrefsRepository : GameRepository {
+
 		public void Save(Score score) {
 			PlayerPrefs.SetInt("Cookies", score.playerTotalCookies);
 			PlayerPrefs.SetInt("CookiesIncrement", score.cookiesIncrementAmmount);
 		}
+
 	}
 }
