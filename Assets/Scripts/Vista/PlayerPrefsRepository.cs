1afa7b233176e27b737075bb65b9f3f56445fda1
Geri <ggasconmoline@gmail.com>
2024-02-10 19:10:41 +0100

    feat: Load from player prefs

--- Assets/Scripts/Vista/PlayerPrefsRepository.cs
+++ Assets/Scripts/Vista/PlayerPrefsRepository.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 namespace Vista {
 	public class PlayerPrefsRepository : GameRepository {
-        public Score LoadGame()
-        {
-            return new Score(51,10);
+        public Score LoadGame() {
+	        int totalCookies = PlayerPrefs.GetInt("Cookies", new Score().playerTotalCookies);
+	        int cookiesIncrement = PlayerPrefs.GetInt("CookiesIncrement", new Score().cookiesIncrementAmmount);
+            return new Score(totalCookies,cookiesIncrement);
         }
 
         public void Save(Score score) {
